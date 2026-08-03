c5e8160e3ddacce5584681a20a02a511a892ea5a
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-08-03 11:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const inputs = document.querySelectorAll('.controls input');
-
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
-
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-
 const video = document.querySelector(".flex");
 const playerButton = document.querySelector(".player__button");
 const progressFilled = document.querySelector(".progress__filled");
@@ -19,40 +9,34 @@ const volume = document.querySelector("#volume");
 const volumeSlider = document.querySelector(".volume-slider");
 const currentTimeOfVideo = document.querySelector(".current-time");
 const timeLine = document.querySelector('.timeline');
-
+const speed= document.querySelector('#speed');
+const playbackSpeed = document.querySelector('.playback__speed');
 video.controls = false;
 let traceTrack = null;
 let totalDuration = null;
 
-console.log("currrent time ", video.currentTime);
 play.addEventListener("click", (event) => {
   pause.classList.remove("toggle");
   play.classList.add("toggle");
-  console.log("play button");
   video.play();
   if (traceTrack) {
     video.currentTime = traceTrack;
-    console.log("After pausing the video ", video.currentTime);
   }
-  // timeline update
 });
 
 pause.addEventListener("click", (event) => {
   pause.classList.add("toggle");
   play.classList.remove("toggle");
-  console.log("stop button");
   video.pause();
 });
 
 rewind.addEventListener("click", (event) => {
-  console.log("inside rewind click ");
   if (traceTrack - 10 >= 0) traceTrack -= 10;
   else traceTrack = 0;
   video.currentTime = traceTrack;
 });
 
 skipForward.addEventListener("click", (event) => {
-  console.log("inside keep forward click ");
   if (traceTrack + 25 <= video.duration) traceTrack += 25;
   else traceTrack = video.duration;
   video.currentTime = traceTrack;
@@ -65,32 +49,34 @@ video.addEventListener("loadedmetadata", (event) => {
 });
 
 video.addEventListener("ended", (event) => {
-  console.log(
-    "Video stopped either because it has finished playing or no further data is available.",
-  );
   traceTrack = null;
   video.currentTime = 0;
   pause.classList.add("toggle");
   play.classList.remove("toggle");
 });
 
-// time update
 video.addEventListener("timeupdate", (event) => {
-  console.log("current video time ", video.currentTime);
   traceTrack = video.currentTime;
   let time = Math.floor(video.currentTime);
   if (time >= 0 && time <= 9) currentTimeOfVideo.textContent = `00:0${time}`;
   else currentTimeOfVideo.textContent = `00:${time}`;
-  
   let percent = (video.currentTime / video.duration) * 100;
   timeLine.style.width = `${percent}%`;
-  console.log("Percentage of video ", percent);
 });
 
 volume.addEventListener("click", (event) => {
   volumeSlider.classList.remove("show");
-  console.log("video volume ",video.volume);
+  volumeSlider.addEventListener('change',(event)=>{
+    let vol = parseFloat(event.target.value)*0.01;
+    video.volume = vol;
+    volumeSlider.classList.add('show');
+  })
 });
 
-
-
+speed.addEventListener('click',(event)=>{
+  playbackSpeed.classList.remove('show');
+  playbackSpeed.addEventListener('change',(event)=>{
+    video.playbackRate = parseFloat(event.target.value);
+    playbackSpeed.classList.add('show');
+  })
+})
